72d12a126ea2271fc9d23d072f9b0242d242bd22
Sunil Kumar <ssunil.uk@gmail.com>
2021-10-03 15:50:34 +0530

    backend added

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ app.post("/signin", signin);
 app.get("/users", user)
 
 app.get("/", async (req, res) => {
-    return  res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+    res.send('Welcome to backend!');
 })
 
 app.use("/tweets", tweetController)
